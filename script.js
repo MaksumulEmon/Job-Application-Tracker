dc1303b09508415ef0e2938b2554c9c9ca57f9a5
MaksumulEmon <maksumulemon300@gmail.com>
2026-02-22 11:54:34 +0600

    Toggle button, clcik, unclick added

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ let rejectedList = [];
 
 let total = document.getElementById('total');
 let minTotal = document.getElementById('minTotal');
-let iinterview =document.getElementById('iinterview');
-let rrejected =document.getElementById('rrejected');
-// console.log(rrejected);
+let iinterview = document.getElementById('iinterview');
+
+
+const allFilterButton = document.getElementById('all-filter-button');
+const interviewFilterButton = document.getElementById('interview-filter-button');
+const rejectedFilterButton = document.getElementById('rejected-filter-button');
+
 
 const allcardSection = document.getElementById('allCards');
+const mainContainer = document.querySelector('main');
 
-const mainContainer =document.querySelector('main');
-console.log(mainContainer);
+function calculateCount() {
+    total.innerText = allcardSection.children.length;
+    minTotal.innerText = allcardSection.children.length;
 
-function calculateCount (){
-    total.innerText=allcardSection.children.length;
-    minTotal.innerText =allcardSection.children.length;
-
-    iinterview.innerText =interviewList.length;
-    rrejected.innerText =rejectedList.length;
+    iinterview.innerText = interviewList.length;
+    rrejected.innerText = rejectedList.length;
 
 }
 calculateCount();
@@ -26,6 +28,27 @@ calculateCount();
 
 // -------------------------------------
 
-function toggleStyle(id){
-    console.log("click")
+function toggleStyle(id) {
+    // Added Biue Bg for all
+    allFilterButton.classList.remove('bg-[#3B82F6]', 'text-white')
+    interviewFilterButton.classList.remove('bg-[#3B82F6]', 'text-white')
+    rejectedFilterButton.classList.remove('bg-[#3B82F6]', 'text-white')
+
+    //  if any buttons has Black than remove
+    allFilterButton.classList.add('bg-white', 'text-[#323B49]')
+    interviewFilterButton.classList.add('bg-white', 'text-[#323B49]')
+    rejectedFilterButton.classList.add('bg-white', 'text-[#323B49]')
+
+
+    const selected = document.getElementById(id);
+
+    // Added Biue Bg for Cursor
+    selected.classList.remove('bg-white', 'text-[#323B49]');
+    selected.classList.add('bg-[#3B82F6]', 'text-white');
 }
+
+
+
+mainContainer.addEventListener('click', function(event){
+    console.log(event.target.parentNode.parentNode);
+})
